c9c21ad6d5ef0ceb3e7c496f292a8ba843d0fd11
Edward <garmonedward@gmail.com>
2020-09-21 21:35:30 -0500

    downloads and handles requests now

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask,request,jsonify
-#from model.buildscript import populate
+import urllib.request
+from model.buildscript import populate
 
+burl = "https://nothingforevermodels.s3.us-east-2.amazonaws.com/"
 app = Flask(__name__)
+modelFiles = ["MasterTokenizer.pkl" ,"jerry_model.h5","elaine_model.h5","kramer_model.h5"]
 
 @app.route('/')
 def index():
@@ -12,11 +15,12 @@ def handle_script():
     content = request.get_json()
     req_script = content.get("req_script")
     temp = content.get("temp")
-
-    print(temp)
-    print(req_script)
-
-    return "hello"
+    return populate(temp,req_script)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
+
+for filename in modelFiles :
+    print("downloading file : " + filename)
+    urllib.request.urlretrieve(burl + filename, './model/' + filename)
+print("done with model downloads")
