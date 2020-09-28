e5034cdfa3d6430d52b87f36c3db25e0d3c4d613
Edward <garmonedward@gmail.com>
2020-09-28 15:41:41 -0500

    checks if downloaeded

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ def download():
         urllib.request.urlretrieve(burl+name,"./model/"+name)
 
 
+downloaded = False
+
 @app.route('/')
 def index():
     return "Index Page"
@@ -22,15 +24,21 @@ def index():
 def l():
     download()
     load()
+    global downloaded
+    downloaded = True
     return("<p>Loaded model</p>")
 
 
 @app.route('/populateScript',methods=['POST'])
 def handle_script():
-    content = request.get_json()
-    req_script = content.get("req_script")
-    temp = content.get("temp")
-    return populate(temp,req_script)
+    global downloaded
+    if(downloaded):
+        content = request.get_json()
+        req_script = content.get("req_script")
+        temp = content.get("temp")
+        return populate(temp,req_script)
+    else:
+        return("<p>Please run route /loadModel</p>")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
